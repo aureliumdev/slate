ef9c88e850c1c6143733832bc8e40e84c7c090e8
Archy-X <63976867+Archy-X@users.noreply.github.com>
2025-11-18 16:30:06 -0500

    Make ItemClick and PlaceholderInfo extend MenuInfo

--- src/main/java/dev/aurelium/slate/info/PlaceholderInfo.java
+++ src/main/java/dev/aurelium/slate/info/PlaceholderInfo.java
@@ -1,39 +1,22 @@
 package dev.aurelium.slate.info;
 
 import dev.aurelium.slate.Slate;
-import dev.aurelium.slate.builder.GlobalBehavior;
 import dev.aurelium.slate.item.provider.PlaceholderData;
 import dev.aurelium.slate.menu.ActiveMenu;
 import org.bukkit.entity.Player;
 
-import java.util.Locale;
-
 /**
  * Represents contextual information about a placeholder in a menu.
  */
-public class PlaceholderInfo {
+public class PlaceholderInfo extends MenuInfo {
 
-    private final Slate slate;
-    private final Player player;
     private final String placeholder;
-    private final ActiveMenu menu;
     private final PlaceholderData data;
 
     public PlaceholderInfo(Slate slate, Player player, String placeholder, ActiveMenu menu, PlaceholderData data) {
-        this.slate = slate;
-        this.player = player;
+        super(slate, player, menu);
         this.placeholder = placeholder;
-        this.menu = menu;
         this.data = data;
-    }
-
-    /**
-     * Gets the player viewing the menu.
-     *
-     * @return the player
-     */
-    public Player player() {
-        return player;
     }
 
     /**
@@ -47,16 +30,6 @@ public class PlaceholderInfo {
     }
 
     /**
-     * Gets the {@link ActiveMenu} instance of the menu. This can be used to get properties of the menu and
-     * modify the menu while its open.
-     *
-     * @return the active menu
-     */
-    public ActiveMenu menu() {
-        return menu;
-    }
-
-    /**
      * Gets data about the specific placeholder, such as where it is located (display name or lore),
      * the active style, and list data if applicable.
      *
@@ -64,16 +37,5 @@ public class PlaceholderInfo {
      */
     public PlaceholderData data() {
         return data;
-    }
-
-    /**
-     * Gets the locale of the player viewing the menu as defined by the {@link GlobalBehavior#localeProvider()}.
-     * If the locale provider is not set, this will always return {@code Locale.ENGLISH}. This is useful if you
-     * have player-dependent locales.
-     *
-     * @return the locale of the player
-     */
-    public Locale locale() {
-        return slate.getGlobalBehavior().localeProvider().get(player);
     }
 }
--- src/main/java/dev/aurelium/slate/item/ItemClick.java
+++ src/main/java/dev/aurelium/slate/item/ItemClick.java
@@ -1,54 +1,27 @@
 package dev.aurelium.slate.item;
 
 import dev.aurelium.slate.Slate;
-import dev.aurelium.slate.builder.GlobalBehavior;
+import dev.aurelium.slate.info.MenuInfo;
 import dev.aurelium.slate.inv.content.SlotPos;
 import dev.aurelium.slate.menu.ActiveMenu;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Locale;
-
 /**
  * Stores contextual data for when a player clicks an item.
  */
-public class ItemClick {
+public class ItemClick extends MenuInfo {
 
-    private final Slate slate;
-    private final Player player;
     private final InventoryClickEvent event;
     private final ItemStack item;
     private final SlotPos pos;
-    private final ActiveMenu menu;
 
     public ItemClick(Slate slate, Player player, InventoryClickEvent event, ItemStack item, SlotPos pos, ActiveMenu menu) {
-        this.slate = slate;
-        this.player = player;
+        super(slate, player, menu);
         this.event = event;
         this.item = item;
         this.pos = pos;
-        this.menu = menu;
-    }
-
-    /**
-     * Gets the player that clicked the item.
-     *
-     * @return the player
-     */
-    public Player player() {
-        return player;
-    }
-
-    /**
-     * Gets the locale of the player as defined by the {@link GlobalBehavior#localeProvider()}.
-     * If the locale provider is not set, this will always return {@code Locale.ENGLISH}. This is useful if you
-     * have player-dependent locales.
-     *
-     * @return the locale of the player
-     */
-    public Locale locale() {
-        return slate.getGlobalBehavior().localeProvider().get(player);
     }
 
     /**
@@ -76,14 +49,5 @@ public class ItemClick {
      */
     public SlotPos pos() {
         return pos;
-    }
-
-    /**
-     * Gets the active menu the player is viewing.
-     *
-     * @return the active menu
-     */
-    public ActiveMenu menu() {
-        return menu;
     }
 }
